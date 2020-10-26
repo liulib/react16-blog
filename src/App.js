903a15302eaa90e1a37212028a30c55f6353d1e7
lvliu <liulibzz@163.com>
2020-10-26 09:54:42 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Des          :
+ * @Author       : liulib
+ * @Date         : 2020-10-26 09:37:01
+ * @LastEditors  : liulib
+ * @LastEditTime : 2020-10-26 09:47:10
+ */
+import React, { Fragment } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Fragment>
+            <div className="App">
+                <h1>This is React App.</h1>
+            </div>
+        </Fragment>
+    )
 }
 
-export default App;
+export default App

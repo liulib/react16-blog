727ed8d19ad94541167d1a7336a0756a659d7718
lvliu <liulibzz@163.com>
2020-10-26 09:57:42 +0800

    支持sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './assets/style/frame.css'
+import './assets/style/frame.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
